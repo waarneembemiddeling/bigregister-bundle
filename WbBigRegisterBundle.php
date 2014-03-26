6fa4ca9cc8267d98312f2f3510915523eacd9de9
Richard van den Brand <richard@vandenbrand.org>
2014-03-26 15:31:52 +0000

    Added missing CompilerPass, fixed bugs

--- WbBigRegisterBundle.php
+++ WbBigRegisterBundle.php
@@ -2,8 +2,16 @@
 
 namespace Wb\Bundle\BigRegisterBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Wb\Bundle\BigRegisterBundle\DependencyInjection\Compiler\BigRegisterCompilerPass;
 
 class WbBigRegisterBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new BigRegisterCompilerPass());
+    }
+
 }
